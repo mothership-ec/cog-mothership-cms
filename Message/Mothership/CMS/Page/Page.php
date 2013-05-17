dbd57e7fa6307fb1713738180915a47896ecf5ac
Joe Holdcroft <joe@message.co.uk>
2013-05-17 17:33:41 +0100

    Adding Page properties

--- Message/Mothership/CMS/Page/Page.php
+++ Message/Mothership/CMS/Page/Page.php
@@ -2,10 +2,49 @@
 
 namespace Message\Mothership\CMS\Page;
 
+/**
+ * Represents the properties of a single page.
+ *
+ * @author Joe Holdcroft <joe@message.co.uk>
+ */
 class Page
 {
+	public $locale;
+	public $authorship;
+
+	public $id;
+	public $title;
+	public $type;
+	public $publishState;
+	public $publishDateRange;
+	public $slug;
+
+	public $left;
+	public $right;
+	public $depth;
+
+	public $metaTitle;
+	public $metaDescription;
+	public $metaHtmlHead;
+	public $metaHtmlFoot;
+	// do we need to store meta _inherit flags?
+
+	public $visibilitySearch;
+	public $visibilityMenu;
+	public $visibilityAggregator;
+
+	public $password;
+	public $access;
+	public $accessGroups;
+
+	public $commentsEnabled;
+	public $commentsAccess;
+	public $commentsAccessGroups;
+	public $commentsApproval;
+	public $commentsExpiry;
+
 	public function getType()
 	{
-
+		return $this->type;
 	}
 }
