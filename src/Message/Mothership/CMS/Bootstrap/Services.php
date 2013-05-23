bff70df91173c0ca61ba8ebda754a5c8b004097b
Joe Holdcroft <joe@message.co.uk>
2013-05-23 20:26:38 +0100

    Adding create decorator

--- src/Message/Mothership/CMS/Bootstrap/Services.php
+++ src/Message/Mothership/CMS/Bootstrap/Services.php
@@ -8,8 +8,12 @@ class Services implements ServicesInterface
 {
 	public function registerServices($serviceContainer)
 	{
-		$serviceContainer['cms.content_loader'] = $serviceContainer->share(function($c) {
+		$serviceContainer['cms.page.content_loader'] = $serviceContainer->share(function($c) {
 			return new \Message\Mothership\CMS\Page\ContentLoader($c['db.query']);
+		});
+
+		$serviceContainer['cms.page.create'] = $serviceContainer->share(function($c) {
+			return new \Message\Mothership\CMS\Page\Create($c['db.query'], $c['event.dispatcher']);
 		});
 	}
 }
---
+++ src/Message/Mothership/CMS/Page/Create.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Message\Mothership\CMS\Page;
+
+use Message\Mothership\CMS\PageTypeInterface;
+
+use Message\Cog\Event\DispatcherInterface;
+use Message\Cog\DB\Query as DBQuery;
+
+/**
+ * Decorator for creating pages.
+ *
+ * @author Joe Holdcroft <joe@message.co.uk>
+ *
+ * @todo Implement the created_by setting. Is there a service for the current
+ *       user?
+ * @todo Pass the default Locale to Loader when it's instantiated and we have it
+ */
+class Create
+{
+	protected $_query;
+	protected $_eventDispatcher;
+
+	/**
+	 * [__construct description]
+	 *
+	 * @param DBQuery             $query           [description]
+	 * @param DispatcherInterface $eventDispatcher [description]
+	 */
+	public function __construct(DBQuery $query, DispatcherInterface $eventDispatcher)
+	{
+		$this->_query           = $query;
+		$this->_eventDispatcher = $eventDispatcher;
+	}
+
+	public function create(PageTypeInterface $pageType, $title)
+	{
+		$result = $this->_query->run("
+			INSERT INTO
+				page
+			SET
+				created_at = UNIX_TIMESTAMP(),
+				created_by = 0,
+				title      = ?s,
+				type       = ?s
+		", array(
+			$title,
+			$pageType->getName(),
+		));
+
+		$loader = new Loader('the locale thing', $this->_query);
+
+		return $loader->getByID($result->id());
+	}
+}
