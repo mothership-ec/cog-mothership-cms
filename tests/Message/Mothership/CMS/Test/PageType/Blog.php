3b4798d436cd301c68bd391606d104978805f8b0
Joe Holdcroft <joe@message.co.uk>
2013-05-28 12:17:55 +0100

    Playing about with a pretend API for page types creating fields & groups

--- tests/Message/Mothership/CMS/Test/PageType/Blog.php
+++ tests/Message/Mothership/CMS/Test/PageType/Blog.php
@@ -16,8 +16,38 @@ class Blog implements PageTypeInterface
 		return 'A blog page type, for use when unit testing.';
 	}
 
-	public function getFields()
+	public function setFields(\Message\Mothership\CMS\PageTypeFieldFactory $factory)
 	{
-		// crazy shiz happens here.
+		// full style
+		$strapline = new \Message\Mothership\CMS\Field\Type\Text('strapline', 'The catchy strapline');
+		$strapline
+			->setLocalisable(true);
+		// set contextual help
+		// set form field label - will we need to tell the field what it's label? see above
+		// set validation
+		//
+
+		$group = new \Message\Mothership\CMS\Field\Group('promo', 'Promotions');
+		$group
+			->setRepeatable(true, 3, 6)
+			->add(new \Message\Mothership\CMS\Field\Type\Text('title', 'Promotion title'), true)
+			->add(new \Message\Mothership\CMS\Field\Type\RichText('description', 'Promotion description'))
+			->add(new \Message\Mothership\CMS\Field\Type\Link('url', 'Promotion link destination'))
+			->add(new \Message\Mothership\CMS\Field\Type\File('file', 'Promotion background image')); // some way to tell it to only show image files?
+
+		return array($strapline, $group);
+
+		// quick style
+		$factory->addField('text', 'strapline', 'The catchy strapline')
+			->setLocalisable(true);
+
+		$factory->addGroup('promo', 'Promotions')
+			->setRepeatable(true, 3, 6)
+			->add($factory->getField('text', 'title', 'Title'), true)
+			->add($factory->getField('richtext', 'description', 'Description'))
+			->add($factory->getField('link', 'url', 'Link destination'))
+			->add($factory->getField('file', 'image', 'Background image')->setAllowedTypes('image'));
+
+		return $factory;
 	}
 }
