754c55297d2a24b54a326b59e9fff409d283a985
Danny Hannah <dannyhannah87@gmail.com>
2013-06-18 12:19:13 +0100

    Improve the code a little and update docblocks

--- src/Message/Mothership/CMS/Controller/Publish.php
+++ src/Message/Mothership/CMS/Controller/Publish.php
@@ -10,7 +10,7 @@ class Publish extends \Message\Cog\Controller\Controller
 	protected $_pageID;
 
 	/**
-	 * Handle the post request and decide the action
+	 * Handle the form request and decide the action
 	 *
 	 * @param  int $pageID PageID of the page we are updating
 	 *
@@ -19,52 +19,58 @@ class Publish extends \Message\Cog\Controller\Controller
 	public function process($pageID)
 	{
 		$this->_pageID = $pageID;
-
+		// Check the form has been processed
 		if ($data = $this->get('request')->request->get('publish')) {
-			if (isset($data['publish']) && $data['publish'] == 'publish') {
-				// Quick publish
-				$this->_publish();
-			} elseif (isset($data['publish']) && $data['publish'] == 'unpublish') {
-				// Quick unpublish
-				$this->_unpublish();
+			// Decide what to do with the post data
+			if (isset($data['publish'])) {
+				// Check for a quick publish / unpublish submission
+				$data['publish'] == 'publish' ? $this->_publish() : $this->_unpublish();
 			} else {
 				// Save publish dates
 				$this->_savePubishDates($data);
 			}
 		}
-		return $this->redirect($this->generateUrl('ms.cms.edit', array('pageID' => $pageID)));
+		return $this->redirectToRoute('ms.cms.edit', array('pageID' => $pageID));
 	}
 
 	/**
 	 * Use the quick publish on the Edit object
+	 *
+	 * @return  Page 	updated page object
 	 */
 	protected function _publish()
 	{
 		$page = $this->_services['cms.page.loader']->getByID($this->_pageID);
-		$page = $this->_services['cms.page.edit']->publish($page);
+
+		return $this->_services['cms.page.edit']->publish($page);
 	}
 
 	/**
 	 * Use the quick unpublish on the Edit object
+	 *
+	 * @return  Page 	updated page object
 	 */
 	protected function _unpublish()
 	{
 		$page = $this->_services['cms.page.loader']->getByID($this->_pageID);
-		$page = $this->_services['cms.page.edit']->unpublish($page);
+
+		return $this->_services['cms.page.edit']->unpublish($page);
 	}
 
 	/**
 	 * Update the page with the publish ranges
 	 *
-	 * @param  array $data post data from form
+	 * @param  array $data 	post data from form
+	 *
+	 * @return  Page 		updated page object
 	 */
 	protected function _savePubishDates($data)
 	{
 		$page = $this->_services['cms.page.loader']->getByID($this->_pageID);
 		$from = $data['publish-date'] ? new DateTimeImmutable($data['publish-date'] .' '. $data['publish-time']) : null;
-		//var_dump($from); exit;
 		$to = $data['unpublish-date'] ? new DateTimeImmutable($data['unpublish-date'] .' '. $data['unpublish-time']) : null;
 		$page->publishDateRange = new DateRange($from, $to);
-		$page = $this->_services['cms.page.edit']->save($page);
+
+		return $this->_services['cms.page.edit']->save($page);
 	}
 }
