011a6ea3c9cc3d86c1023abfa0ea74b7c003dcd9
Joe Holdcroft <joe@message.co.uk>
2013-06-19 12:41:24 +0100

    Whole bunch of changes and tidying up

--- src/Message/Mothership/CMS/Bootstrap/Routes.php
+++ src/Message/Mothership/CMS/Bootstrap/Routes.php
@@ -8,18 +8,34 @@ class Routes implements RoutesInterface
 {
 	public function registerRoutes($router)
 	{
-		$router['ms.cms']->setPrefix('/cms')->setParent('ms.cp');
-		$router['ms.cms']->add('ms.cms.dashboard', '/', '::Controller:Dashboard#index')
+		$router['ms.cp.cms']->setPrefix('/cms')->setParent('ms.cp');
+
+		$router['ms.cp.cms']->add('ms.cp.cms.dashboard', '/', '::Controller:Dashboard#index')
 			->setFormat('ANY');
-		$router['ms.cms']->add('ms.cms.create', '/create', '::Controller:Create#index')
+
+		$router['ms.cp.cms']->add('ms.cp.cms.create', '/create', '::Controller:Create#index')
 			->setFormat('ANY');
-		$router['ms.cms']->add('ms.cms.create.process', '/create/process', '::Controller:Create#process')
+
+		$router['ms.cp.cms']->add('ms.cp.cms.create.process', '/create', '::Controller:Create#process')
 			->setMethod('POST');
-		$router['ms.cms']->add('ms.cms.edit', '/edit/{pageID}', '::Controller:Edit#index')
+
+		$router['ms.cp.cms']->add('ms.cp.cms.edit', '/edit/{pageID}', '::Controller:Edit#index')
 			->setRequirement('pageID', '\d+');
-		$router['ms.cms']->add('ms.cms.edit.process', '/edit/process/{pageID}', '::Controller:Edit#process')
+
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.process', '/edit/{pageID}', '::Controller:Edit#process')
+			->setRequirement('pageID', '\d+')
 			->setMethod('POST');
-		$router['ms.cms']->add('ms.cms.publish', '/edit/publish/{pageID}', '::Controller:Publish#process')
+
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.publish', '/edit/{pageID}/publish/{csrfHash}', '::Controller:Publishing#publish')
+			->setRequirement('pageID', '\d+')
+			->enableCsrf('csrfHash');
+
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.unpublish', '/edit/{pageID}/unpublish/{csrfHash}', '::Controller:Publishing#unpublish')
+			->setRequirement('pageID', '\d+')
+			->enableCsrf('csrfHash');
+
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.publish_scheduling', '/edit/{pageID}/scheduling', '::Controller:Publishing#schedule')
+			->setRequirement('pageID', '\d+')
 			->setMethod('POST');
 	}
 }
