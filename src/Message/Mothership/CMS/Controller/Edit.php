3aa1ba8ee02ddb3606194123347103c821439b01
Joe Holdcroft <joe@message.co.uk>
2013-06-21 16:16:10 +0100

    Starting on edit attributes form

--- src/Message/Mothership/CMS/Controller/Edit.php
+++ src/Message/Mothership/CMS/Controller/Edit.php
@@ -2,6 +2,8 @@
 
 namespace Message\Mothership\CMS\Controller;
 
+use Message\Mothership\CMS\Page\Authorisation;
+
 class Edit extends \Message\Cog\Controller\Controller
 {
 	/**
@@ -46,10 +48,42 @@ class Edit extends \Message\Cog\Controller\Controller
 		$form = $this->get('form')
 			->setName('attributes')
 			->setMethod('POST')
-			->setAction($this->generateUrl('ms.cp.cms.edit.attributes.action'));
+			->setAction($this->generateUrl('ms.cp.cms.edit.attributes.action', array('pageID' => $pageID)))
+			->setDefaultValues(array(
+				'slug'                  => $page->slug->getLastSegment(),
+				'visibility_menu'       => $page->visibilityMenu,
+				'visibility_search'     => $page->visibilitySearch,
+				'visibility_aggregator' => $page->visibilityAggregator,
+				'access'                => $page->access,
+				'access_groups'         => $page->accessGroups,
+				'tags'                  => implode(', ', $page->tags)
+			));
+
+		$form->add('slug', 'text', $this->trans('ms.cms.attributes.slug.label'))
+			->val()->match('^/[a-z0-9\-]+/$');
+
+		$form->add('visibility_menu', 'checkbox', $this->trans('ms.cms.attributes.visibility.menu.label'));
+		$form->add('visibility_search', 'checkbox', $this->trans('ms.cms.attributes.visibility.search.label'));
+		$form->add('visibility_aggregator', 'checkbox', $this->trans('ms.cms.attributes.visibility.aggregator.label'));
+
+		$form->add('access', 'choice', $this->trans('ms.cms.attributes.access.label'), array('choices' => array(
+			Authorisation::ACCESS_ALL        => $this->trans('ms.cms.attributes.access.options.all'),
+			Authorisation::ACCESS_GUEST      => $this->trans('ms.cms.attributes.access.options.guest'),
+			Authorisation::ACCESS_USER       => $this->trans('ms.cms.attributes.access.options.user'),
+			Authorisation::ACCESS_USER_GROUP => $this->trans('ms.cms.attributes.access.options.group'),
+		)));
+
+		$form->add('access_groups', 'choice', $this->trans('ms.cms.attributes.access_groups.label'), array(
+			'choices'  => $this->get('user.groups')->flatten(),
+			'multiple' => true,
+		))->val()->optional();
+
+		$form->add('tags', 'textarea', $this->trans('ms.cms.attributes.tags.label'))
+			->val()->optional();
 
 		return $this->render('::edit/attributes', array(
 			'page' => $page,
+			'form' => $form,
 		));
 	}
 
