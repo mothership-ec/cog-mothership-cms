297d103f25ac0addb6f68d0efba96f7f9418703b
Joe Holdcroft <joe@message.co.uk>
2013-06-26 15:05:14 +0100

    Converting exception to feedback when deleting a page with child pages. Closes #4

--- src/Message/Mothership/CMS/Controller/Delete.php
+++ src/Message/Mothership/CMS/Controller/Delete.php
@@ -2,19 +2,28 @@
 
 namespace Message\Mothership\CMS\Controller;
 
+use Message\Mothership\CMS\Exception;
+
 class Delete extends \Message\Cog\Controller\Controller
 {
 	public function delete($pageID)
 	{
 		$page = $this->get('cms.page.loader')->getByID($pageID);
 
-		$this->get('cms.page.delete')->delete($page);
+		try {
+			$this->get('cms.page.delete')->delete($page);
 
-		$this->addFlash('success', sprintf(
-			'Page `%s` has been deleted. <a href="%s">Restore</a>',
-			$page->title,
-			$this->generateUrl('ms.cp.cms.restore', array('pageID' => $page->id))
-		));
+			$this->addFlash('success', sprintf(
+				'Page `%s` has been deleted. <a href="%s">Restore</a>',
+				$page->title,
+				$this->generateUrl('ms.cp.cms.restore', array('pageID' => $page->id))
+			));
+		}
+		catch (Exception\Exception $e) {
+			$this->addFlash('error', $e->getMessage());
+
+			return $this->redirectToReferer();
+		}
 
 		return $this->redirectToRoute('ms.cp.cms.dashboard');
 	}
