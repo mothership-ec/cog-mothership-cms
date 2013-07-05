b56fff6454f508213431cd3e8d6d1764c2d38cdd
Joe Holdcroft <joe@message.co.uk>
2013-07-05 12:42:55 +0100

    Adding DateTime

--- src/Message/Mothership/CMS/Field/Type/DateTime.php
+++ src/Message/Mothership/CMS/Field/Type/DateTime.php
@@ -6,15 +6,15 @@ use Message\Mothership\CMS\Field\Field;
 use Message\Cog\Form\Handler;
 
 /**
- * A field for a single date.
+ * A field for a single date & time.
  *
  * @author Joe Holdcroft <joe@message.co.uk>
  */
-class Date extends Field
+class DateTime extends Field
 {
 	public function getFormField(Handler $form)
 	{
-		$form->add($this->getName(), 'date', $this->getLabel(), array(
+		$form->add($this->getName(), 'datetime', $this->getLabel(), array(
 			'attr' => array('data-help-key' => $this->_getHelpKeys()),
 		));
 	}
