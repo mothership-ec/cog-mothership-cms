977518b9eb706682ec6c26246df1a7258f05a4b9
Iris Schaffer <irisSchaffer@gmx.net>
2013-09-19 19:06:12 +0100

    Added getFile Method to File Field

--- src/Message/Mothership/CMS/Field/Type/File.php
+++ src/Message/Mothership/CMS/Field/Type/File.php
@@ -70,6 +70,11 @@ class File extends Field implements ContainerAwareInterface
 		return $this;
 	}
 
+	public function getFile()
+	{
+		return $this->_services['file_manager.file.loader']->getByID((int) $this->_value);
+	}
+
 	// public function getValue()
 	// {
 	// 	return $this->_services['file_manager.file.loader']->getByID((int) $this->_value);
