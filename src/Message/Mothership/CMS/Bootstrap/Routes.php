e3cb8d87835115a76ec8a7cee208800798a002b7
Laurence Roberts <lsjroberts@gmail.com>
2013-11-25 16:16:48 +0000

    Updated routes to use fully qualified namespace

--- src/Message/Mothership/CMS/Bootstrap/Routes.php
+++ src/Message/Mothership/CMS/Bootstrap/Routes.php
@@ -10,90 +10,90 @@ class Routes implements RoutesInterface
 	{
 		$router['ms.cms']->setPrefix('/')->setPriority(-500);
 
-		$router['ms.cms']->add('ms.cms.search', '/search', '::Controller:Frontend#searchResults');
+		$router['ms.cms']->add('ms.cms.search', '/search', 'Message:Mothership:CMS::Controller:Frontend#searchResults');
 
-		$router['ms.cms']->add('ms.cms.frontend', '{slug}', '::Controller:Frontend#renderPage')
+		$router['ms.cms']->add('ms.cms.frontend', '{slug}', 'Message:Mothership:CMS::Controller:Frontend#renderPage')
 			->setRequirement('slug', '[a-z0-9\-\/]+')
 			->setDefault('slug', '/');
 
-		$router['ms.cms']->add('ms.cms.broken_link.action', '/broken_link', '::Controller:Module:Form#brokenLinkAction')
+		$router['ms.cms']->add('ms.cms.broken_link.action', '/broken_link', 'Message:Mothership:CMS::Controller:Module:Form#brokenLinkAction')
 			->setMethod('POST');
 
 		$router['ms.cp.cms']->setPrefix('/content')->setParent('ms.cp');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.dashboard', '/', '::Controller:ControlPanel:Dashboard#index')
+		$router['ms.cp.cms']->add('ms.cp.cms.dashboard', '/', 'Message:Mothership:CMS::Controller:ControlPanel:Dashboard#index')
 			->setFormat('ANY');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.create.action', '/create', '::Controller:ControlPanel:Create#process')
+		$router['ms.cp.cms']->add('ms.cp.cms.create.action', '/create', 'Message:Mothership:CMS::Controller:ControlPanel:Create#process')
 			->setMethod('POST');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.create', '/create', '::Controller:ControlPanel:Create#index')
+		$router['ms.cp.cms']->add('ms.cp.cms.create', '/create', 'Message:Mothership:CMS::Controller:ControlPanel:Create#index')
 			->setFormat('ANY');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.edit.action', '/edit/{pageID}', '::Controller:ControlPanel:Edit#updateTitle')
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.action', '/edit/{pageID}', 'Message:Mothership:CMS::Controller:ControlPanel:Edit#updateTitle')
 			->setRequirement('pageID', '\d+')
 			->setMethod('POST');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.edit.metadata.action', '/edit/{pageID}/metadata', '::Controller:ControlPanel:Edit#metadataAction')
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.metadata.action', '/edit/{pageID}/metadata', 'Message:Mothership:CMS::Controller:ControlPanel:Edit#metadataAction')
 			->setRequirement('pageID', '\d+')
 			->setMethod('POST');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.edit', '/edit/{pageID}', '::Controller:ControlPanel:Edit#index')
+		$router['ms.cp.cms']->add('ms.cp.cms.edit', '/edit/{pageID}', 'Message:Mothership:CMS::Controller:ControlPanel:Edit#index')
 			->setRequirement('pageID', '\d+');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.edit.publish', '/edit/{pageID}/publish/{csrfHash}', '::Controller:ControlPanel:Publishing#publish')
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.publish', '/edit/{pageID}/publish/{csrfHash}', 'Message:Mothership:CMS::Controller:ControlPanel:Publishing#publish')
 			->setRequirement('pageID', '\d+')
 			->enableCsrf('csrfHash');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.edit.publish.force', '/edit/{pageID}/publish/{csrfHash}/{force}', '::Controller:ControlPanel:Publishing#publish')
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.publish.force', '/edit/{pageID}/publish/{csrfHash}/{force}', 'Message:Mothership:CMS::Controller:ControlPanel:Publishing#publish')
 			->setRequirement('pageID', '\d+')
 			->enableCsrf('csrfHash');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.edit.unpublish', '/edit/{pageID}/unpublish/{csrfHash}', '::Controller:ControlPanel:Publishing#unpublish')
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.unpublish', '/edit/{pageID}/unpublish/{csrfHash}', 'Message:Mothership:CMS::Controller:ControlPanel:Publishing#unpublish')
 			->setRequirement('pageID', '\d+')
 			->enableCsrf('csrfHash');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.edit.unpublish.force', '/edit/{pageID}/unpublish/{csrfHash}/{force}', '::Controller:ControlPanel:Publishing#unpublish')
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.unpublish.force', '/edit/{pageID}/unpublish/{csrfHash}/{force}', 'Message:Mothership:CMS::Controller:ControlPanel:Publishing#unpublish')
 			->setRequirement('pageID', '\d+')
 			->enableCsrf('csrfHash');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.edit.publish_scheduling', '/edit/{pageID}/scheduling', '::Controller:ControlPanel:Publishing#schedule')
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.publish_scheduling', '/edit/{pageID}/scheduling', 'Message:Mothership:CMS::Controller:ControlPanel:Publishing#schedule')
 			->setRequirement('pageID', '\d+')
 			->setMethod('POST');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.edit.content.action', '/edit/{pageID}/content', '::Controller:ControlPanel:Edit#contentAction')
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.content.action', '/edit/{pageID}/content', 'Message:Mothership:CMS::Controller:ControlPanel:Edit#contentAction')
 			->setRequirement('pageID', '\d+')
 			->setMethod('POST');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.edit.content', '/edit/{pageID}/content', '::Controller:ControlPanel:Edit#content')
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.content', '/edit/{pageID}/content', 'Message:Mothership:CMS::Controller:ControlPanel:Edit#content')
 			->setRequirement('pageID', '\d+');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.edit.attributes.action', '/edit/{pageID}/attributes', '::Controller:ControlPanel:Edit#attributesAction')
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.attributes.action', '/edit/{pageID}/attributes', 'Message:Mothership:CMS::Controller:ControlPanel:Edit#attributesAction')
 			->setRequirement('pageID', '\d+')
 			->setMethod('POST');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.edit.attributes.slug.force', '/edit/{pageID}/attributes/slug/{slug}/{csrfHash}', '::Controller:ControlPanel:Edit#forceSlugAction')
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.attributes.slug.force', '/edit/{pageID}/attributes/slug/{slug}/{csrfHash}', 'Message:Mothership:CMS::Controller:ControlPanel:Edit#forceSlugAction')
 			->setRequirement('pageID', '\d+')
 			->setMethod('GET')
 			->enableCsrf('csrfHash');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.edit.attributes', '/edit/{pageID}/attributes', '::Controller:ControlPanel:Edit#attributes')
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.attributes', '/edit/{pageID}/attributes', 'Message:Mothership:CMS::Controller:ControlPanel:Edit#attributes')
 			->setRequirement('pageID', '\d+');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.edit.metadata', '/edit/{pageID}/metadata', '::Controller:ControlPanel:Edit#metadata')
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.metadata', '/edit/{pageID}/metadata', 'Message:Mothership:CMS::Controller:ControlPanel:Edit#metadata')
 			->setRequirement('pageID', '\d+');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.delete', '/delete/{pageID}', '::Controller:ControlPanel:Delete#delete')
+		$router['ms.cp.cms']->add('ms.cp.cms.delete', '/delete/{pageID}', 'Message:Mothership:CMS::Controller:ControlPanel:Delete#delete')
 			->setRequirement('pageID', '\d+');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.restore', '/restore/{pageID}/{csrfHash}', '::Controller:ControlPanel:Delete#restore')
+		$router['ms.cp.cms']->add('ms.cp.cms.restore', '/restore/{pageID}/{csrfHash}', 'Message:Mothership:CMS::Controller:ControlPanel:Delete#restore')
 			->setRequirement('pageID', '\d+')
 			->enableCsrf('csrfHash');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.edit.metadata', '/edit/{pageID}/metadata', '::Controller:ControlPanel:Edit#metadata')
+		$router['ms.cp.cms']->add('ms.cp.cms.edit.metadata', '/edit/{pageID}/metadata', 'Message:Mothership:CMS::Controller:ControlPanel:Edit#metadata')
 			->setRequirement('pageID', '\d+');
 
-		$router['ms.cp.cms']->add('ms.cp.cms.search', '/search', '::Controller:ControlPanel:Search#process')
+		$router['ms.cp.cms']->add('ms.cp.cms.search', '/search', 'Message:Mothership:CMS::Controller:ControlPanel:Search#process')
 			->setFormat('GET');
 	}
 }
