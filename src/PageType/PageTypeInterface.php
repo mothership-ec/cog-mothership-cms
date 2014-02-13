4acf7159be7cd4ad3ed1ef8464de4d96af876a70
thomasjthomasj <thomasjmarchant@hotmail.com>
2014-02-13 14:51:53 +0000

    PageTypeInterface extends ContentInterface in Cog

--- src/PageType/PageTypeInterface.php
+++ src/PageType/PageTypeInterface.php
@@ -4,39 +4,15 @@ namespace Message\Mothership\CMS\PageType;
 
 use Message\Cog\Field\Factory;
 use Message\Cog\Field\Group;
+use Message\Cog\Field\ContentInterface;
 
 /**
  * Interface that must be used by all page type objects.
  *
  * @author Joe Holdcroft <joe@message.co.uk>
  */
-interface PageTypeInterface
+interface PageTypeInterface extends ContentInterface
 {
-	/**
-	 * Get the identifying name of this page type.
-	 *
-	 * These must be unique: if there is more than one page type registered with
-	 * the same name, an error will be thrown.
-	 *
-	 * @return string The page type name
-	 */
-	public function getName();
-
-	/**
-	 * Get a nicely formatted name for this page type that can be displayed to
-	 * the user.
-	 *
-	 * @return string The page type name
-	 */
-	public function getDisplayName();
-
-	/**
-	 * Get a description for this page type.
-	 *
-	 * @return string The page type description
-	 */
-	public function getDescription();
-
 	/**
 	 * Check if this page type allows children pages.
 	 *
@@ -51,12 +27,4 @@ interface PageTypeInterface
 	 * @return string The cog reference
 	 */
 	public function getViewReference();
-
-	/**
-	 * Set the content fields & groups for this page type on a field factory
-	 * instance.
-	 *
-	 * @param  Factory $factory The field factory to use
-	 */
-	public function setFields(Factory $factory);
 }
