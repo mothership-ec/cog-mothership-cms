bd882640f9ef43c64678080a7a493fcf2ac7c06d
Laurence Roberts <lsjroberts@gmail.com>
2014-02-26 11:59:33 +0000

    Added slug validator test stubs

--- tests/Page/SlugValidatorTest.php
+++ tests/Page/SlugValidatorTest.php
@@ -6,5 +6,39 @@ use Message\Mothership\CMS\Page\SlugValidator;
 
 class SlugValidatorTest extends \PHPUnit_Framework_TestCase
 {
+	public function setUp()
+	{
+		$this->_validator = new SlugValidator;
+	}
 
+	public function testUniqueSlug()
+	{
+		$slug = "a-unique-slug";
+
+		$this->_validator->validate($slug);
+	}
+
+	/**
+	 * @expectedException Exception\SlugExistsException
+	 */
+	public function testSlugExists()
+	{
+
+	}
+
+	/**
+	 * @expectedException Exception\HistoricalSlugExistsException
+	 */
+	public function testSlugExistsHistorically()
+	{
+
+	}
+
+	/**
+	 * @expectedException Exception\DeletedSlugExistsException
+	 */
+	public function testSlugExistsOnDeletedPage()
+	{
+
+	}
 }
