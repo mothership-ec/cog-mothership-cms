a1e35d5478bbf86505aee828d620beb7fcc43792
Laurence Roberts <lsjroberts@gmail.com>
2014-02-26 11:43:07 +0000

    Added stub SlugValidator class and test

---
+++ src/Page/SlugValidator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Message\Mothership\CMS\Page;
+
+use Message\Mothership\CMS\Exception;
+use Message\Cog\ValueObject\Slug;
+
+class SlugValidator
+{
+	public function validate($slug)
+	{
+
+	}
+}
---
+++ tests/Page/SlugValidatorTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Message\Mothership\CMS\Test\Page;
+
+use Message\Mothership\CMS\Page\SlugValidator;
+
+class SlugValidatorTest extends \PHPUnit_Framework_TestCase
+{
+
+}
