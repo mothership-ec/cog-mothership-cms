2384ec8900a9e6f2dfd32e55be0d4b360886db28
thomasjthomasj <thomasjmarchant@hotmail.com>
2014-05-06 17:34:39 +0100

    actually, remove those aliases anyway

--- src/Bootstrap/Services.php
+++ src/Bootstrap/Services.php
@@ -124,16 +124,6 @@ class Services implements ServicesInterface
 			);
 		});
 
-		//// For backwards compatibility with CMS
-		$service['cms.field.factory'] = function($c) {
-			return $c['field.factory'];
-		};
-
-		//// For backwards compatibility with CMS
-		$services['cms.field.form'] = function($c) {
-			return $c['field.form'];
-		};
-
 		$services->extend('field.collection', function($fields, $c) {
 			$fields->add(new \Message\Mothership\CMS\FieldType\Link($c['validator']));
 
