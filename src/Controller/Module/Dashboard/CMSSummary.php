6d0ef39c91569b5b41e0cb4df600c08acdaf28ff
Laurence Roberts <lsjroberts@gmail.com>
2014-06-02 11:01:16 +0100

    Removed caching from cms summary dashboard

--- src/Controller/Module/Dashboard/CMSSummary.php
+++ src/Controller/Module/Dashboard/CMSSummary.php
@@ -11,9 +11,6 @@ use Message\Cog\Controller\Controller;
  */
 class CMSSummary extends Controller
 {
-	const CACHE_KEY = 'dashboard.module.cms-summary';
-	const CACHE_TTL = 3600;
-
 	const UPDATED_COUNT = 4;
 	const DELETED_COUNT = 3;
 
@@ -26,33 +23,27 @@ class CMSSummary extends Controller
 	 */
 	public function index()
 	{
-		if (false === $data = $this->get('cache')->fetch(self::CACHE_KEY)) {
-			$updated = $deleted = [];
+		$count = self::UPDATED_COUNT;
+		$updated = $this->get('db.query')->run("
+			SELECT page_id, title, updated_at
+			FROM page
+			ORDER BY updated_at DESC
+			LIMIT ?i
+		", [$count]);
 
-			$count = self::UPDATED_COUNT;
-			$updated = $this->get('db.query')->run("
-				SELECT page_id, title, updated_at
-				FROM page
-				ORDER BY updated_at DESC
-				LIMIT ?i
-			", [$count]);
+		$count = self::DELETED_COUNT;
+		$deleted = $this->get('db.query')->run("
+			SELECT page_id, title, deleted_at
+			FROM page
+			WHERE deleted_at > 0
+			ORDER BY deleted_at DESC
+			LIMIT ?i
+		", [$count]);
 
-			$count = self::DELETED_COUNT;
-			$deleted = $this->get('db.query')->run("
-				SELECT page_id, title, updated_at
-				FROM page
-				WHERE deleted_at > 0
-				ORDER BY deleted_at DESC
-				LIMIT ?i
-			", [$count]);
-
-			$data = [
-				'updated' => $updated->transpose('page_id'),
-				'deleted' => $deleted->transpose('page_id')
-			];
-
-			$this->get('cache')->store(self::CACHE_KEY, $data, self::CACHE_TTL);
-		}
+		$data = [
+			'updated' => $updated->transpose('page_id'),
+			'deleted' => $deleted->transpose('page_id')
+		];
 
 		return $this->render('Message:Mothership:CMS::modules:dashboard:cms-summary', $data);
 	}
