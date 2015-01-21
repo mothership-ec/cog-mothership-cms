6d51147474b40922214108a22027a4c635c1e901
Samuel Trangmar-Keates <sam@message.co.uk>
2015-01-21 11:34:43 +0000

    Social controller and views

---
+++ src/Controller/Module/Social.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Message\Mothership\CMS\Controller\Module;
+
+use Message\Cog\Controller\Controller;
+use Message\Mothership\CMS\Page\Page;
+use Message\Cog\Filesystem\File;
+
+class Social extends Controller
+{
+	public function share(Page $page, $description = null, File $image = null)
+	{
+		$schemeAndHost = $this->get('http.request.master')->getSchemeAndHttpHost();
+		$uri           = $schemeAndHost . $this->generateUrl('ms.cms.frontend', array('slug' => ltrim($page->slug, '/')));
+
+		return $this->render('Message:Mothership:CMS::modules:social:share', [
+			'networks'    => $this->get('cfg')->social,
+			'uri'         => $uri,
+			'title'       => $page->metaTitle ?: $page->title,
+			'description' => $description ?: $page->metaDescription,
+			'imageUri'    => $image ? $schemeAndHost . $image->getUrl() : null,
+		]);
+	}
+
+	public function links()
+	{
+		return $this->render('Message:Mothership:CMS::modules:social:links', [
+			'networks' => $this->get('cfg')->social,
+		]);
+	}
+}
