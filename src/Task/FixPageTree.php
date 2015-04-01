5555c03851319071ade506590237adb38a358016
Samuel Trangmar-Keates <sam@message.co.uk>
2015-04-01 13:26:15 +0100

    Added feedback

--- src/Task/FixPageTree.php
+++ src/Task/FixPageTree.php
@@ -19,14 +19,31 @@ class FixPageTree extends Task
 		$this->_pageLoader->includeDeleted(true);
 		$this->_pageLoader->includeUnpublished(true);
 
+		$this->writeln('<info>Building page tree</info>');
 		$tree = $this->_buildTree();
+		$this->writeln('<info>Clearing tree data</info>');
 		$this->_query->run($this->_clearTreeDataQuery());
+		$this->writeln('<info>Refreshing nested set data</info>');
 		$this->_buildNestedSet($tree);
+		$this->writeln('<info>Refreshing orphaned pages</info>');
+		$this->_resetOrphanedPages();
+	}
+
+	private function _resetOrphanedPages()
+	{
+		$orphanedPages = $this->_query->run('SELECT `page_id` FROM `page` WHERE `position_left` IS NULL;')->flatten();
+		$this->writeln(count($orphanedPages) . ' orphaned pages found');
+
+		foreach ($orphanedPages as $page) {
+			$this->writeln('Reseting page ' . $page);
+			$this->_nestedSetHelper->insertChildAtEnd($page, null, true)->commit();
+		}
 	}
 
 	private function _buildNestedSet($node)
 	{
 		foreach ($node->children as $child) {
+			$this->writeln('Inserting page ' . $child->page . ' into parent ' . ($node->page ?: 'root'));
 			$this->_nestedSetHelper->insertChildAtEnd($child->page, $node->page, true)->commit();
 			$this->_buildNestedSet($child);
 		}
