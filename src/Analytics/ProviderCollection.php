058f2384eb75084b506b72f58827ce5cd834206f
Samuel Trangmar-Keates <sam@message.co.uk>
2015-10-19 15:41:46 +0100

    More documentation

--- src/Analytics/ProviderCollection.php
+++ src/Analytics/ProviderCollection.php
@@ -4,8 +4,16 @@ namespace Message\Mothership\CMS\Analytics;
 
 use Message\Cog\ValueObject\Collection;
 
+/**
+ * @author Samuel Trangmar-Keates
+ *
+ * A collection for analytics providers
+ */
 class ProviderCollection extends Collection
 {
+	/**
+	 * {@inheritDoc}
+	 */
 	protected function _configure()
 	{
 		$this->setType('\\Message\\Mothership\\CMS\\Analytics\\AnalyticsProvidorInterface');
